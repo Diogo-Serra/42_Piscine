b7db1a635cee0161a75f1c62e79e041c799e69cb
Di Code <di-code@outlook.com>
2025-08-25 14:05:40 +0100

    Add files via upload

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 21:24:00 by diserra           #+#    #+#             */
+/*   Updated: 2025/08/24 21:44:34 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char	src[] = "World!";
+	char	dest[13] = "Hello ";
+	ft_strcpy(dest, src);
+	ft_putstr(dest);
+	write(1 , "\n", 1);
+	return (0);
+}*/
