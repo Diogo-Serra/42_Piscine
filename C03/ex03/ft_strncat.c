056e1bd061cef2814e328b9ea833868bed800587
Di Code <di-code@outlook.com>
2025-08-25 15:19:57 +0100

    Add files via upload

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 14:54:57 by diserra           #+#    #+#             */
+/*   Updated: 2025/08/25 15:02:41 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	if (nb == 0)
+	{
+		return (dest);
+	}
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char	source[] = "World!";
+	char	dest[13] = "Hello ";
+	ft_strncat(dest, source, 6);
+	ft_putstr(dest);
+	return (0);
+}*/
