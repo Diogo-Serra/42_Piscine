bc9d3d144afe9790ed0c61d0794a4e78de085061
Di Code <di-code@outlook.com>
2025-08-26 15:29:49 +0100

    Add files via upload

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 14:41:27 by diserra           #+#    #+#             */
+/*   Updated: 2025/08/26 15:16:04 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	ft_putstr(argv[0]);
+	write(1, "\n", 1);
+}
