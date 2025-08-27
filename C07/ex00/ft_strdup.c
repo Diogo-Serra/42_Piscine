8bbf9077e4fc9ac53cdd5434a6bf3e32f2710a46
Di Code <di-code@outlook.com>
2025-08-27 04:16:23 +0100

    Add files via upload

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 00:43:30 by diserra           #+#    #+#             */
+/*   Updated: 2025/08/27 02:20:22 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc((ft_strlen(src) * sizeof(char)) + 1);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main(int argc, char **argv)
+{
+	if (argc == 1 || argc > 2)
+	{
+		write(1, "Arguments Error!", 16);
+	}
+	if (argc == 2)
+	{
+		ft_putstr(ft_strdup(argv[1]));
+	}
+	return (0);
+}*/
