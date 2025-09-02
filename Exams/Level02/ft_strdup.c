d5793bcf86be35ce4321276403e6868c55b665c4
Di Code <di-code@outlook.com>
2025-09-02 15:43:13 +0100

    added new exercises

--- Exams/Level02/ft_strdup.c
+++ Exams/Level02/ft_strdup.c
@@ -6,10 +6,61 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 15:25:24 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/02 15:26:05 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/02 15:42:03 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	char	*dest;
+
+	if (!src)
+		return (NULL);
+	dest = malloc(ft_strlen(src) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char	*dest;
+
+	dest = ft_strdup("Hello");
+	ft_putstr(dest);
+	write(1, "\n", 1);
+	return (0);
+}
